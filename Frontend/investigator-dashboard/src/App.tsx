113f37195ee6be4054b6f40ec43cb81bd6c554cf
Arnav <arnavr.5610@gmail.com>
2026-09-13 12:08:22 +0530

    feat: add protect routes

--- Frontend/investigator-dashboard/src/App.tsx
+++ Frontend/investigator-dashboard/src/App.tsx
@@ -2,67 +2,75 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Cases from "./pages/Cases";
 import CaseDetails from "./pages/CaseDetails";
-
 import Account from "./pages/Account";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public route */}
         <Route path="/login" element={<Login />} />
 
-        <Route
-          path="*"
-          element={
-            <div className="app">
-              <Sidebar />
+        {/* Protected routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route
+            path="*"
+            element={
+              <div className="app">
+                <Sidebar />
 
-              <main className="main-content">
-                <Navbar />
+                <main className="main-content">
+                  <Navbar />
 
-                <Routes>
-                  <Route
-                    path="/"
-                    element={<Navigate to="/dashboard" replace />}
-                  />
+                  <Routes>
+                    <Route
+                      path="/"
+                      element={
+                        <Navigate to="/dashboard" replace />
+                      }
+                    />
 
-                  <Route
-                    path="/dashboard"
-                    element={<Dashboard />}
-                  />
+                    <Route
+                      path="/dashboard"
+                      element={<Dashboard />}
+                    />
 
-                  <Route
-                    path="/cases"
-                    element={<Cases />}
-                  />
+                    <Route
+                      path="/cases"
+                      element={<Cases />}
+                    />
 
-                  <Route
-                    path="/cases/:caseId"
-                    element={<CaseDetails />}
-                  />
+                    <Route
+                      path="/cases/:caseId"
+                      element={<CaseDetails />}
+                    />
 
-                  
+                    <Route
+                      path="/account"
+                      element={<Account />}
+                    />
 
-                  <Route
-  path="/account"
-  element={<Account />}
-/>
-
-                  <Route
-                    path="*"
-                    element={<Navigate to="/dashboard" replace />}
-                  />
-
-                </Routes>
-              </main>
-            </div>
-          }
-        />
+                    <Route
+                      path="*"
+                      element={
+                        <Navigate
+                          to="/dashboard"
+                          replace
+                        />
+                      }
+                    />
+                  </Routes>
+                </main>
+              </div>
+            }
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
